bd8d91602ef8fa0d887f40fb25f59de4de42759c
hassaanali723 <hassaanali723@gmail.com>
2026-05-26 15:02:54 +0500

    add microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import './globals.css'
 
@@ -185,6 +186,16 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wx383m5xrf");
+          `}
+        </Script>
       </head>
       <body className={jakarta.className} suppressHydrationWarning>
         {children}
